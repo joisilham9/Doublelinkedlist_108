cbf7ec6122ac901c74b6c4d032aaed80c36de986
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-25 15:31:04 +0700

    membuat class node

---
+++ Double_linkedlist/Double_linkedlist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string name;
+	Node* next;
+	Node* prev;
+};
+
+class Doublelinkedlist {
+private: 
+	Node* START;
+public:
+	Doublelinkedlist();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void travese();
+};
