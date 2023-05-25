b8de2a9122fa2f9bad55b97da4156314c42feecd
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-25 15:47:42 +0700

    menbuat method newNode

--- Double_linkedlist/Double_linkedlist.cpp
+++ Double_linkedlist/Double_linkedlist.cpp
@@ -21,4 +21,41 @@ public:
 	bool deleteNode(int rollNo);
 	bool listEmpty();
 	void travese();
+	void revtraverse();
+	void hapus();
+	void searchData();
 };
+
+Doublelinkedlist::Doublelinkedlist() {
+	START = NULL;
+}
+
+void Doublelinkedlist::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the studennt :";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim;// step2
+	newNode->name = nm; //step 2
+
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START-> noMhs){
+		cout << "\nDuplicate number not allowed" << endl;
+		return;
+	}
+	newNode->next = START;
+	if (START != NULL)
+		START->prev = newNode;
+	newNode->prev = NULL;
+	START = newNode;
+	return;
+    }
+
+	Node* current = START;
+	Node* previous = NULL;
+	while (current->next != NULL && current->next->noMhs < nim)
+}
